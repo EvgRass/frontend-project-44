1194ea83cd683d30ea55b7911dab351f39d32d17
Evgeniy Rassolka <johnrass@yandex.ru>
2023-02-18 13:18:30 +0300

    modified:   src/cli.js modified:   src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,39 +1,41 @@
-import {MAXRAUNDS, welcome, getAnswer, wrongAnswer} from '../index.js';
+import { MAXRAUNDS, welcome, getAnswer, wrongAnswer } from '../index.js';
 
 const calc = () => {
-    const name = welcome();
+  const name = welcome();
 
-    console.log('What is the result of the expression?');
+  console.log('What is the result of the expression?');
 
-    for (let i = 0; i < MAXRAUNDS; i += 1) {
-        const number1 = Math.floor(Math.random() * 10);
-        const number2 = Math.floor(Math.random() * 10);
-        const signs = '+-*';
-        const sign = signs[Math.floor(Math.random() * (signs.length))]
-        const corAnswer = calculation(number1, number2, sign);
+  for (let i = 0; i < MAXRAUNDS; i += 1) {
+    const number1 = Math.floor(Math.random() * 10);
+    const number2 = Math.floor(Math.random() * 10);
+    const signs = '+-*';
+    const sign = signs[Math.floor(Math.random() * signs.length)];
+    const corAnswer = calculation(number1, number2, sign);
 
-        const answer = getAnswer(`${number1} ${sign} ${number2}`);
+    const answer = getAnswer(`${number1} ${sign} ${number2}`);
 
-        if (answer === String(corAnswer)) {
-            console.log('Correct!');
-        } else {
-            wrongAnswer(name, answer, corAnswer);
-            return;
-        }        
+    if (answer === String(corAnswer)) {
+      console.log('Correct!');
+    } else {
+      wrongAnswer(name, answer, corAnswer);
+      return;
     }
+  }
 
-    console.log(`Congratulations, ${name}!`);
-}
+  console.log(`Congratulations, ${name}!`);
+};
 
 const calculation = (number1, number2, sign) => {
-    switch (sign) {
-        case '+':
-            return number1 + number2;
-        case '-':
-            return number1 - number2;
-        case '*':
-            return number1 * number2;
-    }
+  switch (sign) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`Unknown sign: '${sign}'`);
+  }
 };
 
 export default calc;
